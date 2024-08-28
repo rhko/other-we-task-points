47be924acf5437377ad1ee0bab54631f1caccb94
rhko <hk.tfr91@gmail.com>
2024-08-28 14:08:59 +0300

    point_4-and_add_docs

--- point-2/PostController.php
+++ point-2/PostController.php
@@ -7,6 +7,10 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    /**
+     * Refactor index action
+     * pass posts directly on view instead of variable creation and use pagination for better performance
+     */
     public function index()
     {
         return view('posts.index', [
@@ -14,6 +18,12 @@ class PostController extends Controller
         ]);
     }
 
+    /**
+     * Refactor store action
+     * 1- create FormRequest for seperate validation logic
+     * 2- use mass assignment to create post with validated data if validation passes
+     * 3- flash message to notify that create proccess success
+     */
     public function store(StorePostRequest $request)
     {
         Post::create($request->validated());
--- point-3/optimizing.php
+++ point-3/optimizing.php
@@ -1,9 +1,23 @@
 <?php
 
-$posts = Post::all();
+
+/**
+ * use eager loading to preload relation, and only select columns we need
+ */
+
+//method 1
+$posts = Post::select(['id', 'title', 'body'])
+            ->with(['comments' => function ($query) {
+                $query->select('id', 'post_id', 'body');
+            }])
+            ->get();
+
+//method 2
+$posts = Post::select(['id', 'title', 'body'])
+            ->with(['comments:id,post_id,body'])
+            ->get();
 
 foreach ($posts as $post) {
-    // This will result in N+1 queries
     foreach ($post->comments as $comment) {
         echo $comment->body;
     }
